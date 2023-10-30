bc338c43b565ccefe0fdcfd8745602723206ff71
Oleksii Bohoslavskyi <al88bo@gmail.com>
2023-10-30 16:20:05 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() =>
+        body.style.backgroundColor = getRandomHexColor(), 1000);
+}
+
+function onStop() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
